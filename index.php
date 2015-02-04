85f51e6e8b562218844e116330dd3999fb58fee6
dprabin <dprabin@yahoo.com>
2015-02-04 10:47:27 +0545

    changed form action to add_contact.php in form and added names to input fields

--- index.php
+++ index.php
@@ -14,28 +14,28 @@
       <h1>Ajax Address Book</h1>
       </div>
       <div class="large-6 columns">
-        <a class="add-btn button right small" data-reveal-id="myModal">Add Contact</a>
-        <div id="myModal" class="reveal-modal" data-reveal>
+        <a class="add-btn button right small" data-reveal-id="addModal">Add Contact</a>
+        <div id="addModal" class="reveal-modal" data-reveal>
           <h2>Add Contact</h2>
-          <form>
+          <form id="addContact" action="#" method="post">
             <div class="row">
-              <div class="large-6 columns"><label>First Name <input type="text" placeholder="Enter First Name" /></label></div>
-              <div class="large-6 columns"><label>Last Name <input type="text" placeholder="Enter Last Name" /></label></div>
+              <div class="large-6 columns"><label>First Name <input name="first_name" type="text" placeholder="Enter First Name" /></label></div>
+              <div class="large-6 columns"><label>Last Name <input name="last_name" type="text" placeholder="Enter Last Name" /></label></div>
             </div>
             <div class="row">
-              <div class="large-4 columns"><label>Email <input type="email" placeholder="Enter Email address" /></label></div>
-              <div class="large-4 columns"><label>Phone Number <input type="text" placeholder="Phone Number" /></label></div>
+              <div class="large-4 columns"><label>Email <input name="email" type="email" placeholder="Enter Email address" /></label></div>
+              <div class="large-4 columns"><label>Phone Number <input name="phone" type="text" placeholder="Phone Number" /></label></div>
               <div class="large-4 columns"><label>Contact Group 
-                <select><option value="family">Family</option><option value="friends">Friends</option><option value="business">Business</option></select>
+                <select name="contact_group"><option value="family">Family</option><option value="friends">Friends</option><option value="business">Business</option></select>
               </label></div>
               <div class="row">
-                <div class="large-6 columns"><label>Address1 <input type="text" placeholder="Enter Street, House Number, GPO" /></label></div>
-                <div class="large-6 columns"><label>Address2 <input type="text" placeholder="Enter Additional Address" /></label></div>
+                <div class="large-6 columns"><label>Address1 <input name="address1" type="text" placeholder="Enter Street, House Number, GPO" /></label></div>
+                <div class="large-6 columns"><label>Address2 <input name="address2" type="text" placeholder="Enter Additional Address" /></label></div>
               </div>
               <div class="row">
-                <div class="large-4 columns"><label>City <input type="email" placeholder="Enter Email address" /></label></div>
+                <div class="large-4 columns"><label>City <input name="city" type="text" placeholder="Enter Email address" /></label></div>
                 <div class="large-4 columns"><label>District
-                  <select>
+                  <select name="district">
                     <option value="Kathmandu">Kathmandu</option>
                     <option value="Lalitpur">Lalitpur</option>
                     <option value="Bhaktapur">Bhaktapur</option>
@@ -44,12 +44,12 @@
                     <option value="Chitwan">Chitwan</option>
                   </select>
                 </label></div>
-                <div class="large-4 columns"><label>Zone/State <input type="text" placeholder="Enter Zone or State" /></label></div>
+                <div class="large-4 columns"><label>Zone/State <input name="state" type="text" placeholder="Enter Zone or State" /></label></div>
             </div>
             <div class="row">
-              <div class="large-12 columns"><label>Notes<textarea placeholder="Enter Optional Notes"></textarea></label></div>
+              <div class="large-12 columns"><label>Notes<textarea name="notes" placeholder="Enter Optional Notes"></textarea></label></div>
             </div>
-            <input type="submit" class="add-btn button right small" value="Submit" />
+            <input name="submit" type="submit" class="add-btn button right small" value="Submit" />
             <a class="close-reveal-modal">&#215;</a>
           </form>
         </div>
