ab86e422718257588e860f909234e37e1187f98d
dprabin <dprabin@yahoo.com>
2015-02-04 10:30:08 +0545

    dynamically generate contacts list

--- contacts.php
+++ contacts.php
@@ -1,3 +1,16 @@
+<?php include('core/init.php'); ?>
+
+<?php 
+//Create DB object
+$db = new Database;
+
+//Run query to select all from contacts
+$db->query("select * from contacts");
+
+//Assign resultset
+$contacts = $db->resultset();
+
+?>
 <div class="row">
   <div class="large-12 columns">
     <table>
@@ -12,42 +25,24 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td><a href="contact.html">Amrit Man Shrestha</a></td>
-          <td>984 139-8888</td>
-          <td>amritms@gmail.com</td>
-          <td><ul><li>55/28 Bagdurbar Marg</li><li>Bagdurbar, Sundhara, Kathmandu, Nepal</li></ul>
-          <td>Friend</td>
-          <td><ul class="button-group radius">
-            <li><a href="#" class="button tiny" data-reveal-id="myModal">Edit</a></li>
-            <li><a href="#" class="button tiny [secondary alert success]">Delete</a></li>
-          </ul></td>
-          </td>
+        <?php foreach($contacts as $contact): ?>
+          <tr>
+            <td><a href="contact.html"><?php echo $contact->first_name." ".$contact->last_name; ?></a></td>
+            <td><?php echo $contact->phone; ?></td>
+            <td><?php echo $contact->email; ?></td>
+            <td><ul>
+              <li><?php echo $contact->address1; ?></li>
+              <li><?php if($contact->address2) echo $contact->address2; ?></li>
+              <li><?php echo $contact->city.", ".$contact->district.", ".$contact->state; ?></li>
+            </ul></td>
+            <td><?php echo $contact->contact_group; ?></td>
+            <td><ul class="button-group radius">
+              <li><a href="#" class="button tiny" data-reveal-id="myModal">Edit</a></li>
+              <li><a href="#" class="button tiny [secondary alert success]">Delete</a></li>
+            </ul></td>
+            </td>
           </tr>
-        <tr>
-          <td><a href="contact.html">Ram Kaji Adhikari</a></td>
-          <td>984 929-1234</td>
-          <td>ramkaji@gmail.com</td>
-          <td><ul><li>House No 33, Kanle Dee</li><li>Bahunbesi, Kathmandu, Nepal</li></ul>
-          <td>Friend</td>
-          <td><ul class="button-group radius">
-            <li><a href="#" class="button tiny" data-reveal-id="myModal">Edit</a></li>
-            <li><a href="#" class="button tiny [secondary alert success]">Delete</a></li>
-          </ul></td>
-          </td>
-          </tr>
-        <tr>
-          <td><a href="contact.html">Trisundhay Paudyal</a></td>
-          <td>9843 012-3456</td>
-          <td>imtrisu@gmail.com</td>
-          <td><ul><li>44/10 Moti Path, Gaheli Tole</li><li>Biratnagar, Morang, Nepal</li></ul>
-          <td>Family</td>
-          <td><ul class="button-group radius">
-            <li><a href="#" class="button tiny" data-reveal-id="myModal">Edit</a></li>
-            <li><a href="#" class="button tiny [secondary alert success]">Delete</a></li>
-          </ul></td>
-          </td>
-        </tr>
+        <?php endforeach; ?>
       </tbody>
     </table>
   </div>
