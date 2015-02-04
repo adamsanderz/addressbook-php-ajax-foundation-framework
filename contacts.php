a0780923d998d80d3eb7ad5569d515963c55cb6d
dprabin <dprabin@yahoo.com>
2015-02-04 09:51:21 +0545

    Contacts list static content

---
+++ contacts.php
@@ -0,0 +1,54 @@
+<div class="row">
+  <div class="large-12 columns">
+    <table>
+      <thead>
+        <tr>
+          <th width="200">Name</th>
+          <th width="130">Phone</th>
+          <th width="200">Email</th>
+          <th width="250">Address</th>
+          <th width="100">Group</th>
+          <th width="150">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><a href="contact.html">Amrit Man Shrestha</a></td>
+          <td>984 139-8888</td>
+          <td>amritms@gmail.com</td>
+          <td><ul><li>55/28 Bagdurbar Marg</li><li>Bagdurbar, Sundhara, Kathmandu, Nepal</li></ul>
+          <td>Friend</td>
+          <td><ul class="button-group radius">
+            <li><a href="#" class="button tiny" data-reveal-id="myModal">Edit</a></li>
+            <li><a href="#" class="button tiny [secondary alert success]">Delete</a></li>
+          </ul></td>
+          </td>
+          </tr>
+        <tr>
+          <td><a href="contact.html">Ram Kaji Adhikari</a></td>
+          <td>984 929-1234</td>
+          <td>ramkaji@gmail.com</td>
+          <td><ul><li>House No 33, Kanle Dee</li><li>Bahunbesi, Kathmandu, Nepal</li></ul>
+          <td>Friend</td>
+          <td><ul class="button-group radius">
+            <li><a href="#" class="button tiny" data-reveal-id="myModal">Edit</a></li>
+            <li><a href="#" class="button tiny [secondary alert success]">Delete</a></li>
+          </ul></td>
+          </td>
+          </tr>
+        <tr>
+          <td><a href="contact.html">Trisundhay Paudyal</a></td>
+          <td>9843 012-3456</td>
+          <td>imtrisu@gmail.com</td>
+          <td><ul><li>44/10 Moti Path, Gaheli Tole</li><li>Biratnagar, Morang, Nepal</li></ul>
+          <td>Family</td>
+          <td><ul class="button-group radius">
+            <li><a href="#" class="button tiny" data-reveal-id="myModal">Edit</a></li>
+            <li><a href="#" class="button tiny [secondary alert success]">Delete</a></li>
+          </ul></td>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
