5903881f797a1369aafec1bb71780073b3401da3
dprabin <dprabin@yahoo.com>
2015-02-04 10:48:38 +0545

    on submitting form, redirect to add_contact, and display addModal using foundation api

--- js/script.js
+++ js/script.js
@@ -4,6 +4,21 @@ $(document).ready(function(){
 
 	//show Contacts on page load
 	showContacts();
+
+	//Add contacts
+	$(document).on('submit','#addContact',function(){
+		//Show Loader Image
+		$('#loaderImage').show();
+
+		//post data from form
+		$.post('add_contact.php',$(this).serialize())
+			.done(function(data){
+				console.log(data);
+				$('#addModal').foundation('reveal','close');
+				showContacts();
+			});
+			return false;
+	});
 });
 
 //Show Contacts function
