d4a07b317a02e67c1fbe4cce3ea23b3b915a4c69
dprabin <dprabin@yahoo.com>
2015-02-05 07:47:18 +0545

    array list of all districts of Nepal

---
+++ helpers/system_helper.php
@@ -0,0 +1,109 @@
+<?php
+/*
+ * Nepalese Districts
+ */
+$districts = array (
+
+	//Gandaki
+	'Gorkha' => 'Gorkha',
+	'Lamjung' => 'Lamjung',
+	'Kaski' => 'Kaski',
+	'Manang' => 'Manang',
+	'Tanahun' => 'Tanahun',
+	'Syangja' => 'Syangja',
+
+	//Dhawalagiri
+	'Baglung' => 'Baglung',
+	'Mustang' => 'Mustang',
+	'Myagdi' => 'Myagdi',
+	'Parbat' => 'Parbat',
+
+	//Bagmati
+	'Bhaktapur' => 'Bhaktapur',
+	'Dhading' => 'Dhading',
+	'Kathmandu'=>'Kathmandu',
+	'Kavrepalanchwok' => 'Kavrepalanchwok',
+	'Lalitpur' => 'Lalitpur',
+	'Nuwakot' => 'Nuwakot',
+	'Rasuwa' => 'Rasuwa',
+	'Sindhupalchowk' => 'Sindhupalchowk',
+
+	//Mahakali
+	'Baitadi' => 'Baitadi',
+	'Dadheldhura' => 'Dadheldhura',
+	'Darchula' => 'Darchula',
+	'Kanchanpur' => 'Kanchanpur',
+
+	//Karnali
+	'Dolpa' => 'Dolpa',
+	'Humla' => 'Humla',
+	'Jumla' => 'Jumla',
+	'Kalikot' => 'Kalikot',
+	'Mugu' => 'Mugu',
+
+	//Seti
+	'Achham' => 'Achham',
+	'Bajhang' => 'Bajhang',
+	'Bajura' => 'Bajura',
+	'Doti' => 'Doti',
+	'Kailali' => 'Kailali',
+
+	//Lumbini
+	'Arghakhanchi' => 'Arghakhanchi',
+	'Gulmi' => 'Gulmi',
+	'Kapilvastu' => 'Kapilvastu',
+	'Nawalparasi' => 'Nawalparasi',
+	'Palpa' => 'Palpa',
+	'Rupandehi' => 'Rupandehi',
+
+	//Mechi
+	'Illam' => 'Illam',
+	'Jhapa' => 'Jhapa',
+	'Panchthar' => 'Panchthar',
+	'Taplejung' => 'Taplejung',
+
+	//Koshi
+	'Bhojpur' => 'Bhojpur',
+	'Dhankuta' => 'Dhankuta',
+	'Morang' => 'Morang',
+	'Sunsari' => 'Sunsari',
+	'Sankhuwasabha' => 'Sankhuwasabha',
+	'Terhathum' => 'Terhathum',
+
+	//Narayani
+	'Bara' => 'Bara',
+	'Chitwan' => 'Chitwan',
+	'Makwanpur' => 'Makwanpur',
+	'Parsa' => 'Parsa',
+	'Rautahat' => 'Rautahat',
+
+	//Rapti
+	'Dang' => 'Dang',
+	'Pyuthan' => 'Pyuthan',
+	'Rolpa' => 'Rolpa',
+	'Rukum' => 'Rukum',
+	'Salyan' => 'Salyan',
+
+	//Sagarmatha
+	'Khotang' => 'Khotang',
+	'Okhaldhunga' => 'Okhaldhunga',
+	'Siraha' => 'Siraha',
+	'Saptari' => 'Saptari',
+	'Solukhumbu' => 'Solukhumbu',
+	'Udaypur' => 'Udaypur',
+
+	//Bheri
+	'Banke' => 'Banke',
+	'Bardiya' => 'Bardiya',
+	'Dailekh' => 'Dailekh',
+	'Jajarkot' => 'Jajarkot',
+	'Surkhet' => 'Surkhet',
+
+	//Janakpur
+	'Dhanusa' => 'Dhanusa',
+	'Dolakha' => 'Dolakha',
+	'Mahottari' => 'Mahottari',
+	'Ramechhap' => 'Ramechhap',
+	'Sarlahi' => 'Sarlahi',
+	'Sindhuli' => 'Sindhuli'
+	);
