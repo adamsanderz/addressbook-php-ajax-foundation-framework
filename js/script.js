2b6dca5d2e2bee483bf2dcb5bb3ca9adecdca7b4
dprabin <dprabin@yahoo.com>
2015-02-05 11:59:54 +0545

    added javascript to handle edit contacts

--- js/script.js
+++ js/script.js
@@ -19,6 +19,21 @@ $(document).ready(function(){
 			});
 			return false;
 	});
+
+	//Edit contact
+	$(document).on('submit','#editContact',function(){
+		//Show Loader Image
+		$('#loaderImage').show();
+
+		//Post data from form
+		$.post("edit_contact.php",$(this).serialize())
+			.done(function(data){
+				console.log(data);
+				$('.editModal').foundation('reveal','close');
+				showContacts();
+			});
+			return false;
+	});
 });
 
 //Show Contacts function
