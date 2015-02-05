3a1fdb2440f3eea2c183bda39472a5eb12583f10
dprabin <dprabin@yahoo.com>
2015-02-05 14:26:32 +0545

    changed submit button value

--- contacts.php
+++ contacts.php
@@ -76,7 +76,7 @@ $contacts = $db->resultset();
                       <div class="large-12 columns"><label>Notes<textarea name="notes" placeholder="Enter Optional Notes"><?php echo $contact->notes; ?></textarea></label></div>
                     </div>
                     <input type="hidden" name="id" value="<?php echo $contact->id; ?>" />
-                    <input name="submit" type="submit" class="add-btn button right small" value="Submit" />
+                    <input name="submit" type="submit" class="add-btn button right small" value="Edit Contact" />
                     <a class="close-reveal-modal">&#215;</a>
                   </form>
                 </div>
