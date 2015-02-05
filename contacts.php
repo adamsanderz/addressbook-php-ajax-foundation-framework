4b2c0d4f8220f287835c6d950323f97f9a81e50f
dprabin <dprabin@yahoo.com>
2015-02-05 07:46:49 +0545

    Insert form and populate values

--- contacts.php
+++ contacts.php
@@ -37,47 +37,49 @@ $contacts = $db->resultset();
             </ul></td>
             <td><?php echo $contact->contact_group; ?></td>
             <td><ul class="button-group radius">
-              <li><a href="#" class="button tiny" data-reveal-id="editModal<?php echo $contact->id; ?>" data-contact-id="<?php echo $contact->id; ?>" >Edit</a></li>
+                <li><a href="#" class="button tiny" data-reveal-id="editModal<?php echo $contact->id; ?>" data-contact-id="<?php echo $contact->id; ?>" >Edit</a></li>
+                <li><a href="#" class="button tiny [secondary alert success]" data-reveal-id="deleteModal<?php echo $contact->id; ?>" data-contact-id="<?php echo $contact->id; ?>" >Delete</a></li>
+              </ul>
                 <div id="editModal<?php echo $contact->id; ?>" data-cid="<?php echo $contact->id; ?>" class="reveal-modal editModal" data-reveal>
                   <h2>Edit Contact</h2>
                   <form id="editContact" action="#" method="post">
                     <div class="row">
-                      <div class="large-6 columns"><label>First Name <input name="first_name" type="text" placeholder="Enter First Name" /></label></div>
-                      <div class="large-6 columns"><label>Last Name <input name="last_name" type="text" placeholder="Enter Last Name" /></label></div>
+                      <div class="large-6 columns"><label>First Name <input name="first_name" type="text" placeholder="Enter First Name" value="<?php echo $contact->first_name; ?>" /></label></div>
+                      <div class="large-6 columns"><label>Last Name <input name="last_name" type="text" placeholder="Enter Last Name" value="<?php echo $contact->last_name; ?>" /></label></div>
                     </div>
                     <div class="row">
-                      <div class="large-4 columns"><label>Email <input name="email" type="email" placeholder="Enter Email address" /></label></div>
-                      <div class="large-4 columns"><label>Phone Number <input name="phone" type="text" placeholder="Phone Number" /></label></div>
+                      <div class="large-4 columns"><label>Email <input name="email" type="email" placeholder="Enter Email address" value="<?php echo $contact->email; ?>" /></label></div>
+                      <div class="large-4 columns"><label>Phone Number <input name="phone" type="text" placeholder="Phone Number" value="<?php echo $contact->phone; ?>" /></label></div>
                       <div class="large-4 columns"><label>Contact Group 
-                        <select name="contact_group"><option value="family">Family</option><option value="friends">Friends</option><option value="business">Business</option></select>
+                        <select name="contact_group">
+                          <option value="Family" <?php echo $contact->group=='Family'?'selected':''; ?> >Family</option>
+                          <option value="Friends" <?php echo $contact->group=='Friends'?'selected':''; ?> >Friends</option>
+                          <option value="Business" <?php echo $contact->group == 'Business'?'selected':''; ?> >Business</option>
+                        </select>
                       </label></div>
                       <div class="row">
-                        <div class="large-6 columns"><label>Address1 <input name="address1" type="text" placeholder="Enter Street, House Number, GPO" /></label></div>
-                        <div class="large-6 columns"><label>Address2 <input name="address2" type="text" placeholder="Enter Additional Address" /></label></div>
+                        <div class="large-6 columns"><label>Address1 <input name="address1" type="text" placeholder="Enter Street, House Number, GPO" value="<?php echo $contact->address1; ?>" /></label></div>
+                        <div class="large-6 columns"><label>Address2 <input name="address2" type="text" placeholder="Enter Additional Address" value="<?php echo $contact->address2; ?>" /></label></div>
                       </div>
                       <div class="row">
                         <div class="large-4 columns"><label>City <input name="city" type="text" placeholder="Enter Email address" /></label></div>
                         <div class="large-4 columns"><label>District
                           <select name="district">
-                            <option value="Kathmandu">Kathmandu</option>
-                            <option value="Lalitpur">Lalitpur</option>
-                            <option value="Bhaktapur">Bhaktapur</option>
-                            <option value="Kaski">Kaski</option>
-                            <option value="Gorkha">Gorkha</option>
-                            <option value="Chitwan">Chitwan</option>
+                            <?php foreach ($districts as $key => $value): ?>
+                              <option value="<?php echo $key; ?>" <?php echo $key==$district?'selected':''; ?> ><?php echo $value?></option>
+                            <?php endforeach; ?>
                           </select>
                         </label></div>
-                        <div class="large-4 columns"><label>Zone/State <input name="state" type="text" placeholder="Enter Zone or State" /></label></div>
+                        <div class="large-4 columns"><label>Zone/State <input name="state" type="text" placeholder="Enter Zone or State" value="<?php echo $contact->state; ?>" /></label></div>
                     </div>
                     <div class="row">
-                      <div class="large-12 columns"><label>Notes<textarea name="notes" placeholder="Enter Optional Notes"></textarea></label></div>
+                      <div class="large-12 columns"><label>Notes<textarea name="notes" placeholder="Enter Optional Notes"><?php echo $contact->notes; ?></textarea></label></div>
                     </div>
                     <input name="submit" type="submit" class="add-btn button right small" value="Submit" />
                     <a class="close-reveal-modal">&#215;</a>
                   </form>
                 </div>
-              <li><a href="#" class="button tiny [secondary alert success]">Delete</a></li>
-            </ul></td>
+              </td>
             </td>
           </tr>
         <?php endforeach; ?>
